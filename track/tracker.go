a71f7660d9141146d2a81250d256c89a94c32003
Zach Taylor <zach.taylor673@gmail.com>
2018-04-07 11:06:05 -0500

    remove httptrack.Match
    this will allow to update tracking timestamp

--- track/tracker.go
+++ track/tracker.go
@@ -5,23 +5,6 @@ import (
 	"ztaylor.me/log"
 )
 
-func Match(name string, addr string) bool {
-	if Service == nil {
-		return false
-	}
-
-	addrs, err := Service.GetAccountAddrs(name)
-	if err != nil {
-		return false
-	}
-	for _, login := range addrs {
-		if login.Addr == addr {
-			return true
-		}
-	}
-	return false
-}
-
 func Addr(addr string) {
 	Pair("", addr)
 }
@@ -30,9 +13,6 @@ func Pair(name string, addr string) {
 	log := log.Add("Name", name).Add("Addr", addr)
 	if Service == nil {
 		log.Warn("httptrack: service is nil")
-		return
-	}
-	if Match(name, addr) {
 		return
 	}
 
