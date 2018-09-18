ec5253bf8b35659b1a04ecd4043428c3b41eb598
Zach Taylor <zach@ztaylor.me>
2018-09-18 12:40:57 -0500

    add package http/handler
    add RedirectHTTPS
    add Index(http.FileSystem) Handler
    add AddPrefix(string, http.Handler) Handler

---
+++ handler/index.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+)
+
+// Index returns a Handler that maps every request to /index.html for injected FileSystem
+func Index(fs http.FileSystem) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		file, err := fs.Open("/index.html")
+		if err != nil {
+			w.Write([]byte("not found"))
+		} else {
+			io.Copy(w, file)
+		}
+		file.Close()
+	})
+}
