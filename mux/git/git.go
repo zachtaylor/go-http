372f95bae7578d88b1460be3bd5cb26af4cfe19b
Zach Taylor <zach@ztaylor.me>
2018-09-18 22:57:53 -0500

    change http/git/Matcher to use MatcherFunc

--- mux/git/git.go
+++ mux/git/git.go
@@ -8,22 +8,13 @@ import (
 	"ztaylor.me/http/mux"
 )
 
-// Matcher provides http/mux.Matcher for http git requests
-type Matcher struct {
-}
-
-// Match provides http/mux.Matcher for http git requests
-func (_ *Matcher) Match(r *http.Request) bool {
+// Matcher returns true for http git requests
+var Matcher = mux.MatcherFunc(func(r *http.Request) bool {
 	if ua := r.Header["User-Agent"][0]; len(ua) > 2 && ua[:3] == "git" {
 		return true
 	}
 	return false
-}
-
-// NewMatcher creates a new Matcher that returns true for http git requests
-func NewMatcher() *Matcher {
-	return nil
-}
+})
 
 // NewHandler creates a new default githttp Handler
 func NewHandler(path string) http.Handler {
@@ -40,10 +31,10 @@ var AuthNoPush = auth.Authenticator(func(info auth.AuthInfo) (bool, error) {
 
 // NewRoute creates a default kind of http git route
 //
-// Uses NewMatcher, NewHandler, AuthNoPush
+// Uses Matcher, NewHandler, AuthNoPush
 func NewRoute(path string) *mux.Route {
 	return &mux.Route{
-		Matcher: NewMatcher(),
+		Matcher: Matcher,
 		Handler: AuthNoPush(NewHandler(path)),
 	}
 }
