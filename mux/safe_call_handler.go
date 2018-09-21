442a64fecd055430b9f3d52b07a1e5e5f9115ec6
Zach Taylor <zach@ztaylor.me>
2018-09-21 10:20:29 -0500

    update to use log.Protect

--- mux/safe_call_handler.go
+++ mux/safe_call_handler.go
@@ -6,18 +6,9 @@ import (
 	"ztaylor.me/log"
 )
 
-// SafeCallHandler uses defer recover() to protect a http.Handler call
+// SafeCallHandler uses log.Protect() to protect a http.Handler call
 func SafeCallHandler(f http.Handler, w http.ResponseWriter, r *http.Request) {
-	defer func() {
-		if e := recover(); e != nil {
-			log.WithFields(log.Fields{
-				"Host":    r.Host,
-				"Path":    r.RequestURI,
-				"Cookies": r.Cookies,
-				"Remote":  r.RemoteAddr,
-				"Error":   e,
-			}).Error("mux: recover")
-		}
-	}()
-	f.ServeHTTP(w, r)
+	log.Protect(func() {
+		f.ServeHTTP(w, r)
+	})
 }
