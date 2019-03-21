0aec4574a04b16ec14a68d6674f88090e5ec8df5
Zach Taylor <zach@ztaylor.me>
2019-03-21 15:35:43 -0500

    repack track, remove globals
    breaks package name

--- track/track.go
+++ track/track.go
@@ -1,1 +1,33 @@
-package httptrack // import "ztaylor.me/http/track"
+package track // import "ztaylor.me/http/track"
+
+import "time"
+
+// LoginDetails is a login event record
+type LoginDetails struct {
+	Name string
+	Addr string
+	time.Time
+}
+
+// Service is a storage connection for login details
+type Service interface {
+	GetAccountAddrs(name string) ([]*LoginDetails, error)
+	SaveAccountAddr(name string, addr string, t time.Time) error
+}
+
+// Cache provides in-heap implementation of Service
+type Cache map[string][]*LoginDetails
+
+// GetAccountAddrs retrieves all login details
+func (c Cache) GetAccountAddrs(name string) ([]*LoginDetails, error) {
+	if c[name] == nil {
+		c[name] = make([]*LoginDetails, 0)
+	}
+	return c[name], nil
+}
+
+// SaveAccountAddr adds a new login detail
+func (c Cache) SaveAccountAddr(name string, addr string, t time.Time) error {
+	c[name] = append(c[name], &LoginDetails{name, addr, t})
+	return nil
+}
