9bfd8064136b0122bf7fc1957cbb591bac8771dd
Zach Taylor <zach@ztaylor.me>
2019-04-10 11:06:44 -0500

    simplify websocket.Service

--- websocket/cache.go
+++ websocket/cache.go
@@ -2,7 +2,6 @@ package websocket
 
 import (
 	"errors"
-	"sync"
 
 	"golang.org/x/net/websocket"
 	"ztaylor.me/http/sessions"
@@ -11,15 +10,19 @@ import (
 // ErrSocketKeyExists is returned by Cache.Store when the key is duplicate
 var ErrSocketKeyExists = errors.New("socket key exists")
 
-// Cache manages the websockets
+// Cache implements Service
 type Cache struct {
 	sessions sessions.Service
 	cache    map[string]*T
 	mux      Mux
-	sync.Mutex
 }
 
-// NewCache creates a Cache with internals initialized
+// cacheIsService is a type check
+func cacheIsService(c *Cache) Service {
+	return c
+}
+
+// NewCache builds a Cache, required for internals
 func NewCache(sessions sessions.Service) *Cache {
 	return &Cache{
 		sessions: sessions,
@@ -28,53 +31,40 @@ func NewCache(sessions sessions.Service) *Cache {
 	}
 }
 
-// New creates a Socket
-func (cache *Cache) New(conn *websocket.Conn) *T {
+// Connect connects a websocket
+func (c *Cache) Connect(conn *websocket.Conn) {
 	t := New(conn)
-	if cache.sessions != nil {
-		if s := cache.sessions.Cookie(conn.Request()); s != nil {
+	if c.sessions != nil {
+		if s := c.sessions.Cookie(conn.Request()); s != nil {
 			t.Session = s
 		}
-		cache.Store(t.String(), t)
 	}
-	return t
+	c.cache[t.Key()] = t
+	c.watch(t) // start monitor
+}
+func (c *Cache) watch(t *T) {
+	for {
+		msg, err := t.NextMessage()
+		if err != nil {
+			delete(c.cache, t.Key())
+			t.Close()
+			return
+		}
+		go c.ServeWS(t, msg)
+	}
 }
 
 // Count returns the number of open sockets
-func (cache *Cache) Count() int {
-	return len(cache.cache)
-}
-
-// Get returns a socket with the given id, if available
-func (cache *Cache) Get(key string) *T {
-	return cache.cache[key]
-}
-
-// Store saves a socket to the cache
-func (cache *Cache) Store(key string, socket *T) (err error) {
-	cache.Lock()
-	if cache.cache[key] == nil {
-		cache.cache[key] = socket
-	} else {
-		err = ErrSocketKeyExists
-	}
-	cache.Unlock()
-	return
-}
-
-// Remove deletes a socket from the cache
-func (cache *Cache) Remove(key string) {
-	cache.Lock()
-	delete(cache.cache, key)
-	cache.Unlock()
+func (c *Cache) Count() int {
+	return len(c.cache)
 }
 
 // Plugin adds a plugin to the mux
-func (cache *Cache) Plugin(p Plugin) {
-	cache.mux = append(cache.mux, p)
+func (c *Cache) Plugin(p Plugin) {
+	c.mux = append(c.mux, p)
 }
 
 // ServeWS calls the mux ServeWS
-func (cache *Cache) ServeWS(t *T, m *Message) {
-	cache.mux.ServeWS(t, m)
+func (c *Cache) ServeWS(t *T, m *Message) {
+	c.mux.ServeWS(t, m)
 }
--- websocket/websocket.go
+++ websocket/websocket.go
@@ -14,18 +14,16 @@ import (
 // Service provides websocket server functionality
 type Service interface {
 	Handler
-	// New creates a Socket
-	New(string) *T
+	// New connects a websocket
+	Connect(*websocket.Conn)
+	// Count returns the number of open sockets
 	Count() int
-	Get(string) *T
-	Grant(string) *T
-	Revoke(string)
 }
 
 // UpgradeHandler provides a websocket handshake func
 func UpgradeHandler(s Service) http.Handler {
 	return websocket.Handler(func(c *websocket.Conn) {
-		New(c).watch(s)
+		s.Connect(c)
 	})
 }
 
@@ -44,6 +42,13 @@ func New(conn *websocket.Conn) *T {
 	}
 }
 
+// Key is RemoteAddr
+//
+// Since websockets block remote ports, it is guaranteed to be unique
+func (t *T) Key() string {
+	return t.conn.Request().RemoteAddr
+}
+
 // GetUser returns a non-empty string value to represent this websocket
 func (t *T) GetUser() string {
 	if t != nil && t.Session != nil {
@@ -55,9 +60,17 @@ func (t *T) GetUser() string {
 	}
 }
 
-// Done is an observable channel that indicates the socket has been closed
+// Done is an observable channel that closes when the socket has been closed
 func (t *T) Done() <-chan bool {
 	return t.done
+}
+
+// Close closes the observable channel
+func (t *T) Close() {
+	if t.done != nil {
+		close(t.done)
+		t.done = nil
+	}
 }
 
 // Write sends a buffer to the underlying websocket connection
@@ -67,25 +80,10 @@ func (t *T) Write(s []byte) {
 	}
 }
 
-func (t *T) String() string {
-	return "ws(" + t.GetUser() + ")://" + t.conn.Request().RemoteAddr
-}
+var errSocketClosed = errors.New("websocket closed")
 
-func (t *T) watch(service Service) {
-	for {
-		msg, err := t.nextMessage()
-		if err != nil {
-			service.Revoke(t.String())
-			close(t.done)
-			return
-		}
-		service.ServeWS(t, msg)
-	}
-}
-
-var errSocketClosed = errors.New("socket closed")
-
-func (t *T) nextMessage() (*Message, error) {
+// NextMessage reads a Message from the socket API
+func (t *T) NextMessage() (*Message, error) {
 	s, msg := "", &Message{}
 	if t == nil || t.conn == nil {
 		return nil, errSocketClosed
@@ -96,4 +94,8 @@ func (t *T) nextMessage() (*Message, error) {
 	}
 	msg.User = t.GetUser()
 	return msg, nil
+}
+
+func (t *T) String() string {
+	return "ws(" + t.GetUser() + ")://" + t.conn.Request().RemoteAddr
 }
