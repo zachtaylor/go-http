a9de22506b3d418e328bee26841ede924a4d9ac4
Zach Taylor <zach@ztaylor.me>
2020-05-07 20:03:34 -0500

    fixes stateless acme challenge verification

--- mux/acme/acme.go
+++ mux/acme/acme.go
@@ -7,10 +7,18 @@ import (
 	"ztaylor.me/http/router"
 )
 
-// Path creates a new *mux.Route for the given file system path to use for ACME challenges on route "/.well-known/acme-challenge/"
-func Path(path string) *mux.Route {
+// Path creates a new *mux.Route for the given file system path to use for stateless ACME challenges on route "/.well-known/acme-challenge/"
+func Path(thumbprint, path string) *mux.Route {
+	lencut := 28 // len("/.well-known/acme-challenge/")
+	addthumb := "." + thumbprint
 	return &mux.Route{
-		Router:  router.Path("/.well-known/acme-challenge/"),
-		Handler: http.FileServer(http.Dir(path)),
+		Router: router.PathStarts("/.well-known/acme-challenge"),
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if len(r.URL.Path) < lencut {
+				return
+			}
+			match := r.URL.Path[lencut:]
+			w.Write([]byte(match + addthumb))
+		}),
 	}
 }
